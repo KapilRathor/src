ebe7574315159279392b69f49e0ae84b82f5821f
KapilRathor <kg.rathor@gmail.com>
2022-09-24 17:07:49 +0530

    Add files via upload
    update usps

--- dataloaders/usps.py
+++ dataloaders/usps.py
@@ -1,75 +1,55 @@
 import os,sys
 import numpy as np
 import torch
-import utils
 from torchvision import datasets,transforms
-from sklearn.utils import shuffle
 
-def get(data_path,seed=0,pc_valid=0.10):
+########################################################################################################################
+
+def get(data_path,seed,fixed_order=False,pc_valid=0):
     data={}
     taskcla=[]
     size=[1,16,16]
 
-    path = os.path.join(data_path, 'binary_usps')
-    if not os.path.isdir(path):
-        os.makedirs(path)
+    # usps
+    mean=(0.1307,)
+    std=(0.3081,)
+    dat={}
+    dat['train']=datasets.USPS(data_path,train=True,download=True,
+                                transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean,std)]))
+    dat['test']=datasets.USPS(data_path,train=False,download=True,
+                               transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean,std)]))
+    data[0]={}
+    data[0]['name']='usps-0-4'
+    data[0]['ncla']=5
+    data[1]={}
+    data[1]['name']='usps-5-9'
+    data[1]['ncla']=5
+    for s in ['train','test']:
+        loader=torch.utils.data.DataLoader(dat[s],batch_size=1,shuffle=False,drop_last=True)
+        data[0][s]={'x': [],'y': []}
+        data[1][s]={'x': [],'y': []}
+        for image,target in loader:
+            label=target.numpy()[0]
+            if label<5:
+                data[0][s]['x'].append(image)
+                data[0][s]['y'].append(label)
+            else:
+                data[1][s]['x'].append(image)
+                data[1][s]['y'].append(label-5)
 
-        mean=[x/255 for x in [125.3,123.0,113.9]]
-        std=[x/255 for x in [63.0,62.1,66.7]]
 
-        # CIFAR10
-        dat={}
-        dat['train']=datasets.USPS(data_path,train=True,download=True,transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean,std)]))
-        dat['test']=datasets.USPS(data_path,train=False,download=True,transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean,std)]))
-        for n in range(5):
-            data[n]={}
-            data[n]['name']='usps'
-            data[n]['ncla']=2
-            data[n]['train']={'x': [],'y': []}
-            data[n]['test']={'x': [],'y': []}
+
+    # "Unify" and save
+    for n in [0,1]:
         for s in ['train','test']:
-            loader=torch.utils.data.DataLoader(dat[s],batch_size=1,shuffle=False)
-            for image,target in loader:
-                n=target.cpu().numpy()[0]
-                nn=n//2
-                data[nn][s]['x'].append(image)
-                data[nn][s]['y'].append(n%2)
-
-        # "Unify" and save
-        for t in data.keys():
-            for s in ['train','test']:
-                data[t][s]['x']=torch.stack(data[t][s]['x']).view(-1,size[0],size[1],size[2])
-                data[t][s]['y']=torch.LongTensor(np.array(data[t][s]['y'],dtype=int)).view(-1)
-                torch.save(data[t][s]['x'], os.path.join(os.path.expanduser(path),'data'+str(t)+s+'x.bin'))
-                torch.save(data[t][s]['y'], os.path.join(os.path.expanduser(path),'data'+str(t)+s+'y.bin'))
-
-    # Load binary files
-    data={}
-    ids=list(shuffle(np.arange(10),random_state=seed))
-    print('Task order =',ids)
-    for i in range(10):
-        data[i] = dict.fromkeys(['name','ncla','train','test'])
-        for s in ['train','test']:
-            data[i][s]={'x':[],'y':[]}
-            data[i][s]['x']=torch.load(os.path.join(os.path.expanduser(path),'data'+str(ids[i])+s+'x.bin'))
-            data[i][s]['y']=torch.load(os.path.join(os.path.expanduser(path),'data'+str(ids[i])+s+'y.bin'))
-        data[i]['ncla']=len(np.unique(data[i]['train']['y'].numpy()))
-        data[i]['name']='usps-'+str(ids[i])
-            
-
+            data[n][s]['x']=torch.stack(data[n][s]['x']).view(-1,size[0],size[1],size[2])
+            data[n][s]['y']=torch.LongTensor(np.array(data[n][s]['y'],dtype=int)).view(-1)
 
     # Validation
     for t in data.keys():
-        r=np.arange(data[t]['train']['x'].size(0))
-        r=np.array(shuffle(r,random_state=seed),dtype=int)
-        nvalid=int(pc_valid*len(r))
-        ivalid=torch.LongTensor(r[:nvalid])
-        itrain=torch.LongTensor(r[nvalid:])
         data[t]['valid']={}
-        data[t]['valid']['x']=data[t]['train']['x'][ivalid].clone()
-        data[t]['valid']['y']=data[t]['train']['y'][ivalid].clone()
-        data[t]['train']['x']=data[t]['train']['x'][itrain].clone()
-        data[t]['train']['y']=data[t]['train']['y'][itrain].clone()
+        data[t]['valid']['x']=data[t]['train']['x'].clone()
+        data[t]['valid']['y']=data[t]['train']['y'].clone()
 
     # Others
     n=0
@@ -79,3 +59,5 @@ def get(data_path,seed=0,pc_valid=0.10):
     data['ncla']=n
 
     return data,taskcla,size
+
+########################################################################################################################
